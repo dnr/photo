a63384811c80f138d42eac6093a10461ec806abf
David Reiss <davidn@gmail.com>
2015-05-30 17:00:13 -0700

    fix fixdates.py

--- fixdates.py
+++ fixdates.py
@@ -2,7 +2,7 @@
 
 import os, re, datetime
 
-DELTA = datetime.timedelta(hours=9)
+DELTA = datetime.timedelta(hours=0)
 
 CHECK_BYTES = 4096 # works for panasonic G3
 # 2015:04:11 00:51:31
@@ -10,18 +10,22 @@ PAT = re.compile(r'\d\d\d\d:\d\d:\d\d \d\d:\d\d:\d\d')
 PAT_LEN = 19
 FMT = '%Y:%m:%d %H:%M:%S'
 
-for fn in os.listdir('.'):
+for fn in sorted(os.listdir('.')):
   if not (fn.endswith('.JPG') or fn.endswith('.RW2')): continue
-  f = open(fn)
+  f = open(fn, 'rb+')
   page = f.read(CHECK_BYTES)
+  unixts = None
   for offset in xrange(len(page)):
     s = page[offset:offset+PAT_LEN]
     m = PAT.match(s)
     if m:
       d = datetime.datetime.strptime(s, FMT) + DELTA
-      d = d.strftime(FMT)
-      assert len(d) == len(s)
-      print '%s @ %5d  %s  ->  %s' % (fn, offset, s, d)
+      n = d.strftime(FMT)
+      assert len(s) == len(n)
+      print '%s @ %5d  %s  ->  %s' % (fn, offset, s, n)
       f.seek(offset)
-      f.write(d)
-
+      f.write(n)
+      unixts = int(d.strftime('%s'))
+  f.close()
+  if unixts:
+    os.utime(fn, (unixts, unixts))
